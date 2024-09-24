241d8bfb12ee7ce00d6ecab1f4247736fae34650
Rafael Ferreira <61987062+rafaesin@users.noreply.github.com>
2024-09-24 20:12:05 +0100

    feat(buts): Add var

--- buts/main.tf
+++ buts/main.tf
@@ -12,6 +12,11 @@ variable "number_var" {
   default = 10
 }
 
+variable "henrique_var" {
+  type = number
+  default = 15
+}
+
 resource "random_integer" "random_int" {
   min = 0
   max = var.number_var
